d54c8a29f66aef8792bc9dec7fe9f7e11551310f
materekh <87072420+materekh@users.noreply.github.com>
2024-02-16 20:55:36 +0400

    commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let main=document.getElementById("main")
+main.innerHTML="<p id='text'>hello</p>"
+setTimeout(()=>{
+    main.innerHTML="<p id='text'>why are you here</p>"
+},2000)
+setTimeout(()=>{
+    main.innerHTML="<p id='text'>just go</p>"
+},4000)
+setTimeout(()=>{
+    main.innerHTML="<p id='text'>nobody is here anyways</p>"
+},6000)
+setTimeout(()=>{
+    main.innerHTML="<p id='text'>why are you still here?</p>"
+},80000)
+setTimeout(()=>{
+    main.innerHTML="<p id='text'>go write a code or something</p>"
+},10000)
+setTimeout(()=>{
+    main.innerHTML="<p id='text'>you're not going are you?</p>"
+},12000)
